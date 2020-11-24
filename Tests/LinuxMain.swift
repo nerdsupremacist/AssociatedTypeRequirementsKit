b91b6006aeec2607b1534e04af993ab7b62babd9
Mathias Quintero <me@quintero.io>
2020-11-24 01:09:46 +0100

    Adding explicit linux main

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,8 @@
-// A LinuxMain.swift file is no longer needed since `swift test --enable-test-discovery` is possible
-// Provide an error message when testing on Linux with no automatic test discovery
-#error("""
-  -----------------------------------------------------
-  Please test with `swift test --enable-test-discovery`
-  -----------------------------------------------------
-  """)
+import XCTest
+
+import AssociatedTypeRequirementsKitTests
+
+var tests = [XCTestCaseEntry]()
+tests += AssociatedTypeRequirementsKitTests.__allTests()
+
+XCTMain(tests)
